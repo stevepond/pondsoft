5871c801913ebbe88263747c4599cdabf582e816
Stephen Pond <sapond@gmail.com>
2019-11-24 12:58:19 -0600

    foo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
+import resume from './resume-icon-16.png';
 import './App.css';
 
 const App: React.FC = () => {
@@ -7,16 +8,13 @@ const App: React.FC = () => {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://drive.google.com/open?id=0B4YjDL0Rk3RYM1NZRkJCS0RTQ1NnNjNaMUNQYklYeGpNQnpz"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+        <img src={resume} className="App-logo" alt="logo" />
         </a>
       </header>
     </div>
